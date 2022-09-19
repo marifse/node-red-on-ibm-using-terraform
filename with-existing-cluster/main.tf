74bebb51f7e0cd4985640754271fd89eb48f5ed9
marifse <50728232+marifse@users.noreply.github.com>
2022-09-19 16:23:14 +0500

    Update main.tf

--- with-existing-cluster/main.tf
+++ with-existing-cluster/main.tf
@@ -4,22 +4,6 @@ data "ibm_container_cluster_config" "cluster" {
   admin           = true
 }
 
-data "ibm_resource_group" "group" {
-  name = var.resource_group
-}
-
-resource "ibm_container_cluster" "cluster" {
-  name              = var.cluster_name
-  datacenter        = var.datacenter
-  default_pool_size = var.default_pool_size
-  machine_type      = var.machine_type
-  hardware          = var.hardware
-  kube_version      = var.kube_version
-  #public_vlan_id    = var.public_vlan_num
-  #private_vlan_id   = var.private_vlan_num
-  resource_group_id = data.ibm_resource_group.group.id
-}
-
 provider "helm" {
   kubernetes {
     host                   = data.ibm_container_cluster_config.cluster.host
