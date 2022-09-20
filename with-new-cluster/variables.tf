3c4ad98b762dcd20819103a557797594398c0d06
marifse <50728232+marifse@users.noreply.github.com>
2022-09-20 18:49:43 +0500

    Update variables.tf

--- with-new-cluster/variables.tf
+++ with-new-cluster/variables.tf
@@ -1,16 +1,10 @@
-variable "cluster_id" {
-  default = "mycluster-free"
-}
+
 
 variable "ibmcloud_api_key" {
   type        = string
   description = "The IAM API Key for IBM Cloud access (https://cloud.ibm.com/iam/apikeys)"
   default = "shdhgfgwqgqhqjjqkkqqqqqqqqqqqqqqq"
   //wE4hgAjJIYxbnLqKf0kwXj0JmP8hsIqE9lDDV3yJr2vP
-}
-
-variable "chart_value_name" {
-  default = "node-red-v11"
 }
 
 variable "resource_group" {
