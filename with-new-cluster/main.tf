aa00046a2d28423459ab86a9599550934da1e78d
marifse <50728232+marifse@users.noreply.github.com>
2022-09-20 16:57:52 +0500

    Update main.tf

--- with-new-cluster/main.tf
+++ with-new-cluster/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.13.1"
+    }
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.45.1"
+    }
+  }
+}
+
+provider "ibm" {
+  ibmcloud_api_key   = var.ibmcloud_api_key
+}
 data "ibm_resource_group" "group" {
   name = var.resource_group
 }
