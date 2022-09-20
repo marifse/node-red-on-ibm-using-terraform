6c5421312d6d9a1c375b7886140fad659309bb01
marifse <50728232+marifse@users.noreply.github.com>
2022-09-20 16:48:43 +0500

    Update and rename with-new-cluster/main.tf to with-new-cluster/helm/main.tf

--- with-new-cluster/helm/main.tf
+++ with-new-cluster/helm/main.tf
@@ -3,20 +3,6 @@ data "ibm_container_cluster_config" "cluster" {
   cluster_name_id = var.cluster_id
   admin           = true
 }
-data "ibm_resource_group" "group" {
-  name = var.resource_group
-}
-resource "ibm_container_cluster" "cluster" {
-  name              = var.cluster_name
-  datacenter        = var.datacenter
-  default_pool_size = var.default_pool_size
-  machine_type      = var.machine_type
-  hardware          = var.hardware
-  kube_version      = var.kube_version
-  #public_vlan_id    = var.public_vlan_num
-  #private_vlan_id   = var.private_vlan_num
-  resource_group_id = data.ibm_resource_group.group.id
-}
 
 provider "helm" {
   kubernetes {
